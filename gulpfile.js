beee4d19fea5ef6ca23dca7f3ca9151b218bf453
Bastien Calou <bastien.calou@gmail.com>
2016-01-23 23:55:59 +0100

    [gulp] end less task on error

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp    = require('gulp');
 var less    = require('gulp-less');
 var connect = require('gulp-connect');
 var ts      = require('gulp-typescript');
+var gutil   = require('gulp-util');
 
 var tsProject = ts.createProject('tsconfig.json');
 
@@ -16,7 +17,10 @@ var staticFiles = [
 
 gulp.task('less', function () {
   return gulp.src(appFolder + 'less/**/*.less')
-    .pipe(less())
+    .pipe(less().on('error', function(err){
+      gutil.log(err);
+      this.emit('end');
+    }))
     .pipe(gulp.dest(distFolder + 'css'))
     .pipe(connect.reload());
 });
