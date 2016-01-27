9fa2c17421e7e1b057513b197aed58e24f44c9e4
Bastien Calou <bastien.calou@gmail.com>
2016-01-28 00:57:02 +0100

    [step11] generate dummy text when clicking on a button

--- app/components/generator.component.ts
+++ app/components/generator.component.ts
@@ -1,9 +1,13 @@
-import {Component}        from 'angular2/core';
+import {Component} from 'angular2/core';
 
 @Component({
   selector: 'matchmaker-generator',
   templateUrl: 'app/templates/generator.html',
 })
 export class GeneratorComponent {
+  result: string;
 
+  generate() {
+    this.result = 'Dummy result';
+  }
 }
