55a009afc79c03d49e1304ba47baced1447a4f91
Bastien Calou <bastien.calou@gmail.com>
2016-01-27 21:24:27 +0100

    [step1] create libraries component and use it in main component

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,7 +1,9 @@
-import {Component} from 'angular2/core';
+import {Component}          from 'angular2/core';
+import {LibrariesComponent} from './libraries.component';
 
 @Component({
     selector: 'matchmaker-app',
-    templateUrl: 'app/templates/app.html'
+    templateUrl: 'app/templates/app.html',
+    directives: [LibrariesComponent]
 })
 export class AppComponent { }
---
+++ app/components/libraries.component.ts
@@ -0,0 +1,9 @@
+import {Component} from 'angular2/core';
+
+@Component({
+  selector: 'matchmaker-libraries',
+  templateUrl: 'app/templates/libraries.html'
+})
+export class LibrariesComponent {
+
+}
