84ae4094187f25ab6962d5b680ea5a53eb99755d
Bastien Calou <bastien.calou@gmail.com>
2016-01-27 23:55:08 +0100

    [step4] create dedicated Library class

---
+++ app/classes/library.ts
@@ -0,0 +1,5 @@
+export class Library {
+  constructor(public title: string) {
+
+  }
+}
--- app/components/libraries.component.ts
+++ app/components/libraries.component.ts
@@ -1,9 +1,14 @@
 import {Component} from 'angular2/core';
+import {Library}   from '../classes/library';
 
 @Component({
   selector: 'matchmaker-libraries',
   templateUrl: 'app/templates/libraries.html'
 })
 export class LibrariesComponent {
-  libraries: Array<String> = ["Personne 1", "Action", "Personne 2"];
+  libraries: Array<Library> = [
+    new Library('Personne 1'),
+    new Library('Action'),
+    new Library('Personne 3')
+  ];
 }
