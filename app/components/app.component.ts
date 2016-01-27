358cefa6ecc69165fd9c360ae2c5db2cd124afcb
Bastien Calou <bastien.calou@gmail.com>
2016-01-28 00:48:35 +0100

    [step10] add generator component

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,9 +1,10 @@
 import {Component}          from 'angular2/core';
 import {LibrariesComponent} from './libraries.component';
+import {GeneratorComponent} from './generator.component';
 
 @Component({
     selector: 'matchmaker-app',
     templateUrl: 'app/templates/app.html',
-    directives: [LibrariesComponent]
+    directives: [LibrariesComponent, GeneratorComponent]
 })
 export class AppComponent { }
---
+++ app/components/generator.component.ts
@@ -0,0 +1,9 @@
+import {Component}        from 'angular2/core';
+
+@Component({
+  selector: 'matchmaker-generator',
+  templateUrl: 'app/templates/generator.html',
+})
+export class GeneratorComponent {
+
+}
